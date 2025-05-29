f305211e5a3ebf9b648ee850507479bb75bef395
Psalmstrings <psalmstrings@gmail.com>
2025-05-29 12:27:09 -0700

    New Add

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,13 +1,12 @@
 const express = require("express")
 const userRouter= express.Router()
-const {getAllUsers, deleteUser,  getUserById, getUserByQuery, updateUser} = require("../controllers/usercontroller")
+const {getAllUsers, getSingleUsers, addUser} = require("../controllers/usercontroller")
 
 
 userRouter.get("/", getAllUsers)
-userRouter.get("/query", getUserByQuery)
-userRouter.get("/:id", getUserById)
-userRouter.delete("/:id", deleteUser)
-userRouter.patch("/:id", updateUser)
+userRouter.get("/", getSingleUsers)
+userRouter.get("/", addUser)
+
 
 // userRouter.post("/", addUser)
 
